ff93664b36c4693e3a50c5060740abdf4f39d1eb
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-07-15 16:30:40 +0200

    feat: add landing page button

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import TypewriterComponent from "typewriter-effect";
+import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
+import { useAuth } from "@clerk/nextjs";
 
 export const LandingHero = () => {
+  const { isSignedIn } = useAuth();
+
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
@@ -27,9 +31,13 @@ export const LandingHero = () => {
       <div className="text-sm md:text-xl font-light text-zinc-400">
         Create content using AI 10x faster.
       </div>
-      <Button variant="premium" className="md:text-lg p-4 md:p-6 rounded-full font-semibold">
-        Start Creating For Free
-      </Button>
+      <div>
+        <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
+          <Button variant="premium" className="md:text-lg p-4 md:p-6 rounded-full font-semibold">
+            Start Generating For Free
+          </Button>
+        </Link>
+      </div>
       <div className="text-zinc-400 text-xs md:text-sm font-normal">
         No credit card required.
       </div>
