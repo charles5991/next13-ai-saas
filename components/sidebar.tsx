3e3086a474a6a3b334b8d8cbed8182b4dbe5a383
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-07-16 04:02:02 +0200

    feat: reorder

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -23,18 +23,6 @@ const routes = [
     color: "text-sky-500"
   },
   {
-    label: 'Conversation',
-    icon: MessageSquare,
-    href: '/conversation',
-    color: "text-violet-500",
-  },
-  {
-    label: 'Music Generation',
-    icon: Music,
-    color: "text-emerald-500",
-    href: '/music',
-  },
-  {
     label: 'Image Generation',
     icon: ImageIcon,
     color: "text-pink-700",
@@ -45,6 +33,18 @@ const routes = [
     icon: VideoIcon,
     color: "text-orange-700",
     href: '/video',
+  },
+  {
+    label: 'Conversation',
+    icon: MessageSquare,
+    href: '/conversation',
+    color: "text-violet-500",
+  },
+  {
+    label: 'Music Generation',
+    icon: Music,
+    color: "text-emerald-500",
+    href: '/music',
   },
   {
     label: 'Code Generation',
